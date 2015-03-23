fbcc37982cdbe2d17dd452094f8a30a220db30bb
ThoRodriguez <rdgz.thomas@gmail.com>
2015-03-23 12:22:33 +0100

    Ajout du fichier tetris.py et de la configuration globale.

---
+++ tetris.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 11:34:18 2015
+
+@author: thorodriguez
+"""
+
+import tkinter as tk
+
+
+def lancement_jeu(monjeu):
+
+    BoutonLancer = tk.Button(monjeu, text ='Lancer le jeu', \
+    command = lancement_jeu)
+    BoutonLancer.pack(padx = 5, pady = 5)
+
+    BoutonQuitter = tk.Button(monjeu, text ='Quitter', command = monjeu.destroy)
+    BoutonQuitter.pack(padx = 5, pady = 5)
+
+def main():
+    monjeu = tk()
+    monjeu.title('Tetris de Thomas et Maxime')
+    monjeu.geometry()
+    lancement_jeu(monjeu)
+
+
+main()
