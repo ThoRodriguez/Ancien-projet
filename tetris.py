d7bbc0e5545c491e1855f88692fb12916d5e461b
ThoRodriguez <rdgz.thomas@gmail.com>
2015-04-06 21:23:53 +0200

    Image cube et code
    J'ai créé une petite image cube qui pourra prendre des couleurs
    différentes avec photoshop.
    Pour le code je l'ai un peu avancé, j'ai réussi à afficher le cube
    transparent sur un fond blanc.

--- tetris.py
+++ tetris.py
@@ -6,26 +6,63 @@ Created on Mon Mar 23 11:34:18 2015
 """
 
 import pygame
+from random import randint
 
 pygame.init()
 
-surf_hauteur = 1208
-surf_largeur = 986
+#couleurs
+rose = (255,192,203)
+jaune = (255,215,0)
+orange = (255,102,0)
+bleu = (102,204,255)
+violet = (204,0,255)
+
+
+moooodoooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+mooooooooom
+
+time.set_timer(KEYDOWN,30)
+surf_hauteur = 600
+surf_largeur = 480
+blanc = (255,255,255)
+noir = (0,0,0)
 
 surface = pygame.display.set_mode((surf_hauteur, surf_largeur)) #taille fenetre
+surface.fill(blanc)
+
+
+cube = pygame.image.load('cubetetris.png')
+position_cube = cube.get_rect()
+surface.blit(cube, (position_cube))
+
+
+pygame.display.update() #rafraichissement
+
 pygame.display.set_caption("Jeu de Thomas et Maxime") #nom fenetre
 
 
-def fenetre_principale():
-    
-    fond = pygame.image.load("fondtetris.jpg").convert
 
+def main ():
     game_over = False
-    while (game_over == False) : 
+    while (game_over == False) :  #quitter boucle inf
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
-    pygame.quit()
-    quit()
+    
 
-fenetre_principale()
+main()
+pygame.quit()
+quit()
